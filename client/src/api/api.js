975ae4779ae6691457060696c5aaa14d903d4145
Akshay Kumar Singh <140572866+Codewithakk@users.noreply.github.com>
2024-09-25 14:42:37 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const local = 'http://localhost:5000'
-const production = ''
+// const local = 'http://localhost:5000'
+const production = 'https://assignment-akshay.onrender.com'
 const api = axios.create({
     baseURL: `${local}/api`,
     withCredentials : true
