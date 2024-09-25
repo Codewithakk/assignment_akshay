a1e9ef9f2a3f3f5f603d8198aa6ffbe898518594
Akshay Kumar Singh <140572866+Codewithakk@users.noreply.github.com>
2024-09-25 18:13:59 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-// const local = 'http://localhost:5000'
-const production = 'https://assignment-akshay.onrender.com'
+ const local = 'http://localhost:5000'
+// const production = 'https://assignment-akshay.onrender.com'
 const api = axios.create({
-    baseURL: `${production}/api`,
+    baseURL: `${local}/api`,
     withCredentials : true
 })
 export default api
