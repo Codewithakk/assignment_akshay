2791621937a1730ddd39c01252858fed6f4b1334
Akshay Kumar Singh <140572866+Codewithakk@users.noreply.github.com>
2024-09-28 16:35:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const socket = require('socket.io')
 const server = http.createServer(app)
 
 app.use(cors({
-    origin: ['https://multivendorsites.netlify.app', 'https://cheerful-sawine-b2d799.netlify.app'],
+    origin: ['https://multivendorsites.netlify.app', 'https://multivendorsiteseller.netlify.app'],
     credentials: true
 }))
 
